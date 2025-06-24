2b18b188c11b7a7e670a62d4daf0a541d350c417
sabreena6 <sabreenayaseen6@gmail.com>
2025-06-24 19:40:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ async def leaderboard(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🏆 Leaderboard:\n" + result)
 
 # Run bot
-if _name_ == "_main_":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
